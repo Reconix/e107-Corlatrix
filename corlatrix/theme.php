3ae7b7942638f232e8c04b6bb14e7217c28398ca
Reconix <Reconix@users.noreply.github.com>
2017-11-11 07:54:37 +0000

    Top right nav with login
    Still need to test social login. ATM we just have a placeholder.

--- corlatrix/theme.php
+++ corlatrix/theme.php
@@ -165,94 +165,48 @@ function tablestyle($caption, $text, $id='', $info=array())
 // Default Header
 $LAYOUT['_header_'] =  <<<TMPL
 
-    <header id="header">
-        <div class="top-bar">
-            <div class="container">
-                <div class="row">
-					<!-- Social Icons -->
-					{XURL_ICONS}
+<header id="header">
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+				<!-- Social Icons -->
+				{XURL_ICONS}
 
-					<!-- Contact Number
-                    <div class="col-sm-3 col-xs-4">
-                        <div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>
-                    </div>
-					-->
+				<!-- Contact Number
+				<div class="col-sm-3 col-xs-4">
+					<div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>
+				</div>
+				-->
 
 				<div class="col-sm-6 col-xs-4">
 
-				<div class="top-right-nav">
-				<ul>
-					<li><a href="#register">Register</a></li>
+					{TOP_RIGHT_NAV}
 
-					<li class="dropdown">
-					  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <span class="caret" style="margin-right:10px;"></span></a>
-						<ul id="login-dp" class="dropdown-menu">
-							<li>
-								 <div class="row">
-										<div class="col-md-12">
-											<p>Social Login</p>
-											<div class="social-buttons">
-												<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-												<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-											</div>
-											<p>Site Login</p>
-											 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-													<div class="form-group">
-														 <label class="sr-only" for="exampleInputEmail2">Email address</label>
-														 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Username / Email" required>
-													</div>
-													<div class="form-group">
-														 <label class="sr-only" for="exampleInputPassword2">Password</label>
-														 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-													</div>
-													<div class="form-group">
-														 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-													</div>
-													<div class="checkbox">
-														 <label>
-														 <input type="checkbox"> keep me logged-in
-														 </label>
-													</div>
-											 </form>
-										</div>
-										<div class="bottom text-center">
-											<div class="help-block text-right"><a href="">Forgotten the password ?</a></div>
-											<div class="help-block text-right"><a href="">Resend verification ?</a></div>
-										</div>
+				</div> <!-- /.col-sm-6 col-xs-4 -->
 
-								 </div>
-							</li>
-						</ul>
-					</li>
-				</ul>
+			</div> <!-- /.row -->
+		</div><!--/.container-->
+	</div><!--/.top-bar-->
 
-				</div>
+    <nav class="navbar navbar-inverse" role="banner">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{SITEURL}"><img src="{SITEURL}e107_themes/corlatrix/images/logo.png" alt="logo"></a>
+            </div>
 
-				</div>
+            <div class="collapse navbar-collapse navbar-right">
+				{NAVIGATION=main}
+            </div>
+        </div><!--/.container-->
+    </nav><!--/nav-->
 
-                </div>
-            </div><!--/.container-->
-        </div><!--/.top-bar-->
-
-        <nav class="navbar navbar-inverse" role="banner">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="{SITEURL}"><img src="{SITEURL}e107_themes/corlatrix/images/logo.png" alt="logo"></a>
-                </div>
-
-                <div class="collapse navbar-collapse navbar-right">
-					{NAVIGATION=main}
-                </div>
-            </div><!--/.container-->
-        </nav><!--/nav-->
-
-    </header><!--/header-->
+</header><!--/header-->
 
 TMPL;
 
--- corlatrix/theme_shortcodes.php
+++ corlatrix/theme_shortcodes.php
@@ -6,7 +6,7 @@
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
- * e107 Bootstrap Theme Shortcodes. 
+ * e107 Bootstrap Theme Shortcodes.
  *
 */
 
@@ -17,7 +17,7 @@ class theme_shortcodes extends e_shortcode
 
 	function __construct()
 	{
-		
+
 	}
 
 /*
@@ -87,16 +87,16 @@ class theme_shortcodes extends e_shortcode
 		}
 
 		e107::includeLan(e_PLUGIN."login_menu/languages/".e_LANGUAGE.".php");
-		
-		$tp = e107::getParser();		   
+
+		$tp = e107::getParser();
 		require(e_PLUGIN."login_menu/login_menu_shortcodes.php"); // don't use 'require_once'.
 
 		$direction = vartrue($parm['dir']) == 'up' ? ' dropup' : '';
-		
+
 		$userReg = defset('USER_REGISTRATION');
-				   
-		if(!USERID) // Logged Out. 
-		{		
+
+		if(!USERID) // Logged Out.
+		{
 			$text = '
 			<ul class="nav navbar-nav navbar-right'.$direction.'">';
 
@@ -115,10 +115,10 @@ class theme_shortcodes extends e_shortcode
 				$text .= '
 				<li class="divider-vertical"></li>
 				<li class="dropdown">
-			
+
 				<a class="dropdown-toggle" href="#" data-toggle="dropdown">'.LAN_LOGINMENU_51.' <strong class="caret"></strong></a>
 				<div class="dropdown-menu col-sm-12" style="min-width:250px; padding: 15px; padding-bottom: 0px;">
-				
+
 				{SOCIAL_LOGIN: size=2x&label=1}
 				'; // Sign In
 			}
@@ -126,16 +126,16 @@ class theme_shortcodes extends e_shortcode
 			{
 				return '';
 			}
-			
-			
-			if(!empty($userReg)) // value of 1 or 2 = login okay. 
+
+
+			if(!empty($userReg)) // value of 1 or 2 = login okay.
 			{
 
-			//	global $sc_style; // never use global - will impact signup/usersettings pages. 
+			//	global $sc_style; // never use global - will impact signup/usersettings pages.
 			//	$sc_style = array(); // remove any wrappers.
 
-				$text .='	
-				
+				$text .='
+
 				<form method="post" onsubmit="hashLoginPassword(this);return true" action="'.e_REQUEST_HTTP.'" accept-charset="UTF-8">
 				<p>{LM_USERNAME_INPUT}</p>
 				<p>{LM_PASSWORD_INPUT}</p>
@@ -144,22 +144,22 @@ class theme_shortcodes extends e_shortcode
 				<div class="form-group"></div>
 				{LM_IMAGECODE_NUMBER}
 				{LM_IMAGECODE_BOX}
-				
+
 				<div class="checkbox">
-				
+
 				<label class="string optional" for="autologin"><input style="margin-right: 10px;" type="checkbox" name="autologin" id="autologin" value="1">
 				'.LAN_LOGINMENU_6.'</label>
 				</div>
 				<input class="btn btn-primary btn-block" type="submit" name="userlogin" id="userlogin" value="'.LAN_LOGINMENU_51.'">
 				';
-				
+
 				$text .= '
-				
+
 				<a href="{LM_FPW_LINK=href}" class="btn btn-default btn-sm  btn-block">'.LAN_LOGINMENU_4.'</a>
 				<a href="{LM_RESEND_LINK=href}" class="btn btn-default btn-sm  btn-block">'.LAN_LOGINMENU_40.'</a>
 				';
-				
-				
+
+
 				/*
 				$text .= '
 					<label style="text-align:center;margin-top:5px">or</label>
@@ -167,34 +167,34 @@ class theme_shortcodes extends e_shortcode
 					<input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Sign In with Twitter">
 				';
 				*/
-				
+
 				$text .= "<p></p>
 				</form>
 				</div>
-				
+
 				</li>
 				";
-			
+
 			}
 
 			$text .= "
-			
-			
-			</ul>";	
-			
-			
-			
-			return $tp->parseTemplate($text, true, $login_menu_shortcodes);
-		}  
 
-		
-		// Logged in. 
-		//TODO Generic LANS. (not theme LANs) 	
+
+			</ul>";
+
+
+
+			return $tp->parseTemplate($text, true, $login_menu_shortcodes);
+		}
+
+
+		// Logged in.
+		//TODO Generic LANS. (not theme LANs)
 
 		$userNameLabel = !empty($parm['username']) ? USERNAME : '';
 
 		$text = '
-		
+
 		<ul class="nav navbar-nav navbar-right'.$direction.'">
 		<li class="dropdown">{PM_NAV}</li>
 		<li class="dropdown dropdown-avatar"><a href="#" class="dropdown-toggle" data-toggle="dropdown">{SETIMAGE: w=30} {USER_AVATAR: shape=circle} '. $userNameLabel.' <b class="caret"></b></a>
@@ -206,24 +206,24 @@ class theme_shortcodes extends e_shortcode
 			<a class="dropdown-toggle no-block" role="button" href="{LM_PROFILE_HREF}"><span class="glyphicon glyphicon-user"></span> '.LAN_LOGINMENU_13.'</a>
 		</li>
 		<li class="divider"></li>';
-		
-		if(ADMIN) 
+
+		if(ADMIN)
 		{
-			$text .= '<li><a href="'.e_ADMIN_ABS.'"><span class="fa fa-cogs"></span> '.LAN_LOGINMENU_11.'</a></li>';	
+			$text .= '<li><a href="'.e_ADMIN_ABS.'"><span class="fa fa-cogs"></span> '.LAN_LOGINMENU_11.'</a></li>';
 		}
-		
+
 		$text .= '
 		<li><a href="'.e_HTTP.'index.php?logout"><span class="glyphicon glyphicon-off"></span> '.LAN_LOGOUT.'</a></li>
 		</ul>
 		</li>
 		</ul>
-		
+
 		';
 
 
 		return $tp->parseTemplate($text,true,$login_menu_shortcodes);
-	}	
-	
+	}
+
 
 	/*
 	 * @example shortcode to render news.
@@ -261,14 +261,138 @@ class theme_shortcodes extends e_shortcode
 	}
 
 
+	// Top Right Nav
+	function sc_top_right_nav($parm='')
+	{
+		include_lan(e_PLUGIN."login_menu/languages/".e_LANGUAGE.".php");
+
+		$tp = e107::getParser();
+		require(e_PLUGIN."login_menu/login_menu_shortcodes.php"); // don't use 'require_once'.
+		$direction = vartrue($parm['dir']) == 'up' ? ' dropup' : '';
+
+		$userReg = defset('USER_REGISTRATION');
+
+		if(!USERID) // Logged Out.
+		{
+			$text = '
+		<div class="top-right-nav">
+			<ul>
+			';
+			if($userReg==1)
+			{
+				$text .= '
+				<li><a href="'.e_SIGNUP.'"><i class="fa fa-user-plus" aria-hidden="true"></i> Register</a></li>
+				'; // Signup
+			}
+			$socialActive = e107::pref('core', 'social_login_active');
+			if(!empty($userReg) || !empty($socialActive)) // e107 or social login is active.
+			{
+				$text .= //Header
+				'
+
+				<li class="dropdown">
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-secret" aria-hidden="true"></i> Login <span class="caret" style="margin-right:10px;"></span></a>
+					<ul id="login-dp" class="dropdown-menu">
+						<li>
+							<div class="row">
+								<div class="col-md-12">
+
+				'; // Sign In
+			}
+			else
+			{
+				return '';
+			}
+
+			if(!empty($userReg)) // value of 1 or 2 = login okay.
+			{
+			//	global $sc_style; // never use global - will impact signup/usersettings pages.
+			//	$sc_style = array(); // remove any wrappers.
+				$text .= //Body
+				'
+				{SOCIAL_LOGIN: size=2x&label=1}
+				<p>Social Login</p>
+				<div class="social-buttons">
+					<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+					<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+				</div>
+				<p>Site Login</p>
+				<form class="form" role="form" method="post" onsubmit="hashLoginPassword(this);return true" action="'.e_REQUEST_HTTP.'" accept-charset="UTF-8" id="login-nav">
+					<div class="form-group">
+						{LM_USERNAME_INPUT}
+					</div>
+					<div class="form-group">
+						{LM_PASSWORD_INPUT}
+					</div>
+					<div class="form-group">
+						<button type="submit" name="userlogin" id="userlogin" class="btn btn-primary btn-block">Sign in</button>
+					</div>
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" name="autologin" id="autologin"> keep me logged-in
+						</label>
+					</div>
+					{LM_IMAGECODE_NUMBER}
+					{LM_IMAGECODE_BOX}
+				</form>
+
+				';
+
+				$text .= //Footer
+				'
+								</div>
+								<div class="bottom text-center">
+									<div class="help-block text-right"><a href="{LM_FPW_LINK=href}">Forgotten Password</a></div>
+									<div class="help-block text-right"><a href="{LM_RESEND_LINK=href}">Resend Activation Link</a></div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+				';
+
+			}
+
+			return $tp->parseTemplate($text, true, $login_menu_shortcodes);
+		}
+
+		// Logged in.
+		//TODO Generic LANS. (not theme LANs)
+		$userNameLabel = !empty($parm['username']) ? USERNAME : '';
+
+		$text .= //PM
+		'
+				<li><a href="messages.html"><span>Private Messages</span></a>
+					<ul class="sub-menu">
+						{PM_NAV}
+						<li><a href="#">Conversation list</a></li>
+						<li><a href="#">Single conversation</a></li>
+						<li><a href="#">Start new conversation</a></li>
+					</ul>
+				</li>
+		';
+		$text .= //Profile
+		'
+				<li><a href="{LM_PROFILE_HREF}"><i style="padding:6px;">{SETIMAGE: w=16} {USER_AVATAR: shape=circle}</i><span> '. $userNameLabel.'Profile </span></a>
+					<ul class="sub-menu">
+						<li><a href="{LM_PROFILE_HREF}">Your Profile</a></li>
+						<li><a href="{LM_USERSETTINGS_HREF}">Settings</a></li>
+						<li><a href="'.e_HTTP.'index.php?logout"> Logout</a></li>
+					</ul>
+				</li>
+
+		';
+
+		if(ADMIN) // Admin Link
+		{
+			$text .= '<li><a href="'.e_ADMIN_ABS.'"> Admin Area</a></li>';
+		}
+
+		return $tp->parseTemplate($text,true,$login_menu_shortcodes);
+	}
 
 
-
-	
 }
-
-
-
-
-
 ?>
