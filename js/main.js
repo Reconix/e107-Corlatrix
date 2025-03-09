ec30dad1ffdbd710c363f0533787d78573f829a5
Reconix <Reconix@users.noreply.github.com>
2025-03-09 19:24:12 +0000

    BS5 Update, first pass ( Foundations )
    The foundations are there. started working from the footer up - still lots todo, would've been so much easier starting from scratch...
    I have no idea how to fix the header, that's why we're starting from the footer haha

--- js/main.js
+++ js/main.js
@@ -1,77 +1,73 @@
-jQuery(function($) {'use strict',
+jQuery(function($) {
+    'use strict';
 
-	//#main-slider
-	$(function(){
-		$('#main-slider.carousel').carousel({
-			interval: 8000
-		});
-	});
+    //#main-slider
+    $('#main-slider.carousel').carousel({
+        interval: 8000
+    });
 
-	// accordian
-	$('.accordion-toggle').on('click', function(){
-		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
+    // accordion
+    $('.accordion-toggle').on('click', function() {
+        $(this).closest('.panel-group').children().each(function() {
+            $(this).find('>.panel-heading').removeClass('active');
+        });
+        $(this).closest('.panel-heading').toggleClass('active');
+    });
 
-	 	$(this).closest('.panel-heading').toggleClass('active');
-	});
+    // Initiate WOW JS
+    new WOW().init();
 
-	//Initiat WOW JS
-	new WOW().init();
-
-	// portfolio filter
-	$(window).on('load', function() {
-		'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-
-		$portfolio.isotope({
-			itemSelector: '.portfolio-item',
-			layoutMode: 'fitRows'
-		});
-
-		$portfolio_selectors.on('click', function() {
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.on('submit', function(event) {
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function() {
-				form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
-			}
-		}).done(function(data) {
-			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
-		});
-	});
-
-	// goto top
-	// Check to see if the window is top if not then display button
-	$(window).on('scroll', function() {
-		if ($(this).scrollTop() > 100) {
-			$('#gototop').fadeIn();
-		} else {
-			$('#gototop').fadeOut();
-		}
-	});
-
-	// Click event to scroll to top
-	$('#gototop').on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({scrollTop: 0}, 900);
-	});
+    // portfolio filter
+	var $portfolio_selectors = $('.portfolio-filter > li > a');
+    var $portfolio = $('.portfolio-items');
+    if ($portfolio.length) { // Only run if portfolio exists
+        $portfolio.isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+        $portfolio_selectors.on('click', function() {
+            $portfolio_selectors.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $portfolio.isotope({ filter: selector });
+            return false;
+        });
+    }
 
 	// Pretty Photo
+	console.log("PrettyPhoto links found:", $("a[rel^='prettyPhoto']").length);
 	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
+		social_tools: false,
+		changepicturecallback: function() { console.log("PrettyPhoto opened!"); }
 	});
+
+    // Contact form
+    var form = $('#main-contact-form');
+    form.on('submit', function(event) {
+        event.preventDefault();
+        var form_status = $('<div class="form_status"></div>');
+        $.ajax({
+            url: $(this).attr('action'),
+            beforeSend: function() {
+                form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
+            }
+        }).done(function(data) {
+            form_status.html('<p class="text-success">' + (data.message || 'Email sent successfully') + '</p>').delay(3000).fadeOut();
+        });
+    });
+
+    // goto top
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $('#gototop').fadeIn();
+        } else {
+            $('#gototop').fadeOut();
+        }
+    });
+
+    $('#gototop').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 900);
+    });
+
 });
